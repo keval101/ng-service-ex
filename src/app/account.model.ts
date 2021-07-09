3c3617be0f759a9b5bfad789ad50a07db935819c
keval101 <kevalvadhiya101@gmail.com>
2021-07-09 15:04:41 +0530

    code

---
+++ src/app/account.model.ts
@@ -0,0 +1,8 @@
+export class Account{
+
+
+    constructor(public name:string, public status:string){
+        this.name = name;
+        this.status = status;
+    }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'servise';
+  accounts = [
+    {
+      name: 'Master Account',
+      status: 'active'
+    },
+  ];
+
+  onAccountAdded(newAccount: {name: string, status: string}) {
+    this.accounts.push(newAccount);
+  }
+
+  onStatusChanged(updateInfo: {id: number, newStatus: string}) {
+    this.accounts[updateInfo.id].status = updateInfo.newStatus;
+  }
 }
